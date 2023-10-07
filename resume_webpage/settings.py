9f0aa33c988b576fbc0e4e8f92643679f8e1b3e0
Lawrence Laugesen <laugesen11@gmail.com>
2023-10-07 14:37:07 -0500

    Further updated security

--- resume_webpage/settings.py
+++ resume_webpage/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
 SECRET_KEY = 'django-insecure-u4m^wt((moz_qm@t3-dws=dt8deui=!i)d&%_fc4lslgm*fi4)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
